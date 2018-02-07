7e60a3016a5d6e663abe64b110f9bfc9642e942c
Darell Hoei <darellhoei@gmail.com>
2018-02-06 23:01:29 -0800

    Created music list

--- app/src/main/java/team20/flashbackmusic/MainActivity.java
+++ app/src/main/java/team20/flashbackmusic/MainActivity.java
@@ -1,15 +1,30 @@
 package team20.flashbackmusic;
 
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private ListView listView;
+    private ListAdapter listAdapter;
+    private List<String> songList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +33,70 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Make list
+        listView = (ListView) findViewById(R.id.songList);
+
+        songList = new ArrayList<>();
+
+        getMusic(songList);
+
+        listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, songList);
+        listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (mediaPlayer != null) {
+                    mediaPlayer.release();
+                }
+
+                int resID = getResources().getIdentifier(songList.get(i), "raw", getPackageName());
+                mediaPlayer = MediaPlayer.create(MainActivity.this, resID);
+                mediaPlayer.start();
+            }
+        });
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //Replace with play the current music played
+                //or just change to pause button if there is no music
+                //selected
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+
     }
+
+
+    public void getMusic(List songList)
+    {
+        Field[] fields = R.raw.class.getFields();
+
+        for (int i = 0; i < fields.length; i++) {
+            songList.add(fields[i].getName());
+        }
+    }
+
+    public void loadMedia(int resourceId) {
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+        }
+
+        AssetFileDescriptor assetFileDescriptor = this.getResources().openRawResourceFd(resourceId);
+        try {
+            mediaPlayer.setDataSource(assetFileDescriptor);
+            mediaPlayer.prepareAsync();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/team20/flashbackmusic/Song.java
@@ -0,0 +1,64 @@
+package team20.flashbackmusic;
+
+/**
+ * Created by dhoei on 06/02/18.
+ */
+
+public class Song {
+
+    //private members of song
+    private String songName, artist, mostRecentDateTime,mostRecentLocation;
+    private int score, status;
+
+
+    //constructor of song
+    public Song(String name, String artist){
+        mostRecentDateTime = null;
+        mostRecentLocation = null;
+
+        //for status, 1 = favorite, 0 = neutral, -1 = dislike
+        //initialize with neutral status
+        status = 0;
+
+        //initialize with score 0 because the music has not been
+        //played
+        score = 0;
+    }
+
+
+    // GETTER AND SETTER METHOD //
+    public String getName()
+    {
+        return songName;
+    }
+
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    public String getRecentDateTime()
+    {
+        return mostRecentDateTime;
+    }
+
+    public String getRecentLoc()
+    {
+        return mostRecentLocation;
+    }
+
+    public void setStatus(int s)
+    {
+        status = s;
+    }
+
+    public int getStatus()
+    {
+        return status;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+}
